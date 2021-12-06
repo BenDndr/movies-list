c2a86b189b497926a83d0c363093956436f8e6e2
Benjamin DANDRE <dandre.ben@gmail.com>
2021-12-06 20:12:40 +0100

    api done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+movies = Movie.create([
+    {
+        title: "Whiplash",
+        image_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3XriEpTdnplQRzyphAC0cu3emns.jpg"
+    },
+    {
+        title: "Shutter Island",
+        image_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fQ0vGVTtxjCdAJnxwPZ88O3Wzrh.jpg"
+    },
+    {
+        title: "Interstellar",
+        image_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1pnigkWWy8W032o9TKDneBa3eVK.jpg"
+    },
+    {
+        title: "Fight Club",
+        image_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jSziioSwPVrOy9Yow3XhWIBDjq1.jpg"
+    },
+    {
+        title: "Batman Dark Knight Rises",
+        image_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ApcGBERN0p9I0nDOIwJeEmpnLU5.jpg"
+    },
+    {
+        title: "Warcraft",
+        image_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/51ghTkfKRttjOk01h3ye14JQFwY.jpg"
+    },
+])
+
+reviews = Review.create([
+    {
+        title: "Good Movie",
+        description: "Loved It !",
+        score: 5,
+        movie: movies.first
+    },
+    {
+        title: "Bad af fk",
+        description: "Hated it !",
+        score: 1,
+        movie: movies.first
+    },
+])
